219bd4352b1ee1d3f8ee483c553aea760cee34ff
alejandrosalgar <alejandro.salgar@gmail.com>
2026-02-12 11:04:47 -0500

    feat: agegar capacidad a bus

--- clases/buses.py
+++ clases/buses.py
@@ -1,7 +1,8 @@
 class Bus:
-    def __init__(self, marca: str, modelo: str):
+    def __init__(self, marca: str, modelo: str, capacidad: int):
         self.marca = marca
         self.modelo = modelo
+        self.capacidad = capacidad
 
     def imprimir_data(self):
         print(f"la marca del carro es {self.marca} y el modelo es {self.modelo}")
